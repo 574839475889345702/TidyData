14e008e88679d54b4f70ae58e67a9fcd9a354294
mike <shortymike007@gmail.com>
2015-08-20 15:02:28 -0700

    added README.md explaining how to set up the script.

---
+++ README.md
@@ -0,0 +1,29 @@
+## Getting the data
+
+The data set to be used by this script can be found at:
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+## Extracting the data
+
+Unzip the file into the same directory as 'run_analysis.R'. Its default name will be "UCI HAR Dataset" but can be changed.  The required files needed for execution are:
+
+  activity_labels.txt
+  features.txt
+  |- test
+     |- subject_test.txt
+     |- X_test.txt
+     |- Y_test.txt
+  |- train
+     |- subject_train.txt
+     |- X_train.txt
+     |- Y_train.txt
+
+## Running the Script
+
+`source("run_analysis.R")`
+`data.frame <- aggregateActivityData(path = "UCI HAR Dataset") #path can be change to where zip was extracted`
+
+## What the Script Does
+
+The aggregateActivityData() will load the test and training datasets and combine them. It will then select the columns that calculate the mean and standard deviation of the measured features. Then the data will be further aggregated to find the mean of all features per subject per activity.
--- run_analysis.R
+++ run_analysis.R
@@ -1,26 +1,26 @@
-aggregateActivityData <- function()
+aggregateActivityData <- function(path = "UCI HAR Dataset")
 {
   ## get activity types, automatically converted to factors
-  ActivityType <- read.table("UCI HAR Dataset/activity_labels.txt")$V2
+  ActivityType <- read.table(paste(path,"activity_labels.txt",sep='/'))$V2
   
   ## get list of data observations types
-  features <- read.table("UCI HAR Dataset/features.txt")$V2
+  features <- read.table(paste(path,"features.txt",sep='/'))$V2
   
   ## find the features corresponding to mean and standard deviation
   index <- sort( c( grep("mean()", features, value = FALSE,fixed = TRUE),
                     grep("std()" , features, value = FALSE,fixed = TRUE) ) )
   
   ## load test data
-  df.test  <<- cbind( read.table("UCI HAR Dataset/test/subject_test.txt")$V1,
-                      factor(read.table("UCI HAR Dataset/test/Y_test.txt")$V1,labels=ActivityType),
-                      read.table("UCI HAR Dataset/test/X_test.txt")[index]
+  df.test  <<- cbind( read.table(paste(path,"test/subject_test.txt",sep='/'))$V1,
+                      factor(read.table(paste(path,"test/Y_test.txt",sep='/'))$V1,labels=ActivityType),
+                      read.table(paste(path,"test/X_test.txt",sep='/'))[index]
                     )
   names(df.test) <- c("subjectID", "activity", as.character( features[index]) )
   
   ## load training data
-  df.train <<- cbind( read.table("UCI HAR Dataset/train/subject_train.txt")$V1,
-                      factor(read.table("UCI HAR Dataset/train/Y_train.txt")$V1,labels=ActivityType),
-                      read.table("UCI HAR Dataset/train/X_train.txt")[index]
+  df.train <<- cbind( read.table(paste(path,"train/subject_train.txt",sep='/'))$V1,
+                      factor(read.table(paste(path,"train/Y_train.txt",sep='/'))$V1,labels=ActivityType),
+                      read.table(paste(path,"train/X_train.txt",sep='/'))[index]
                     )
   names(df.train) <- c("subjectID", "activity", as.character( features[index]) )
   
